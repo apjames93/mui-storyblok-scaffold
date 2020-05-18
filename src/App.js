7a427260e4e042aab6dd6b1cfa6a41e69ef0d742
alex james <apjames93@gmail.com>
2020-05-18 07:44:57 -0600

    cra and then added in mui-storyblok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiStoryblok from 'mui-storyblok/dist/index.mjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Pages = () => (
+  <MuiStoryblok
+    theme={{}}
+    accessToken=""
+    version="draft"
+  />
+);
 
-export default App;
+export default Pages;
+
+Pages.propTypes = {};
